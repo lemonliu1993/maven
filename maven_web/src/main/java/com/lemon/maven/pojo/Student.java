363ee10c7f2734ec45f824ca9de54ee8debd8948
lemonliu1993 <2305247091@qq.com>
2024-09-11 10:21:44 +0800

    maven build

---
+++ maven_web/src/main/java/com/lemon/maven/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.lemon.maven.pojo;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+/**
+ * Created by lemoon on 2024/9/9 23:04
+ */
+public class Student {
+
+    public void sayHello(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+    }
+}
---
+++ maven_web/src/test/java/com/lemon/maven/test/DruidTest.java
@@ -0,0 +1,15 @@
+package com.lemon.maven.test;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.Test;
+
+/**
+ * Created by lemoon on 2024/9/9 23:07
+ */
+public class DruidTest {
+
+    @Test
+    public void testDruid(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+    }
+}
